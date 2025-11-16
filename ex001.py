81c6f3e8ca8aa6aae56f24371dcaaa6491e181bd
andreymsfsantos <62684260+andreymsfsantos@users.noreply.github.com>
2025-11-16 20:10:10 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,2 @@
+Nome=input('qual seu nome? ')
+print('Olá',Nome,'prazer em conhecer ')
---
+++ ex002.py
@@ -0,0 +1,6 @@
+from ex001 import Nome
+
+dia=input('Dia = ')
+mes=input('Mes = ')
+ano=input('Ano = ')
+print(Nome,' você nasceu no dia ',dia,' de ',mes,' de ',ano,' correto?')
